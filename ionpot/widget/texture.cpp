d5d7594a2490ac8bdc6e13a149d925b12d5eda61
Emir Kotan <emirkotan@gmail.com>
2022-04-01 02:01:12 +0100

    widget::text() -> widget::Texture()

--- ionpot/widget/texture.cpp
+++ ionpot/widget/texture.cpp
@@ -4,8 +4,14 @@
 #include "point.hpp"
 #include "size.hpp"
 
+#include <sdl/font.hpp>
+#include <sdl/renderer.hpp>
 #include <sdl/texture.hpp>
 
+#include <util/rgba.hpp>
+
+#include <memory> // std::shared_ptr
+#include <string>
 #include <utility> // std::move
 
 namespace ionpot::widget {
@@ -16,6 +22,21 @@ namespace ionpot::widget {
 
 	Texture::Texture(sdl::Texture&& tx):
 		Texture(std::move(tx), tx.query_size())
+	{}
+
+	Texture::Texture(
+			std::shared_ptr<sdl::Renderer> renderer,
+			const sdl::Font& font,
+			const util::RGBA& color,
+			std::string text
+	):
+		Texture(
+			sdl::Texture {
+				renderer,
+				font.render_blended(text, color)
+			},
+			font.calculate_size(text)
+		)
 	{}
 
 	void
