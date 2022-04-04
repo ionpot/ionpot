33f8ac98805c8da358d572e23040f243c77daae2
Emir Kotan <emirkotan@gmail.com>
2022-04-04 22:18:08 +0100

    PaddedText and PaddedTextBox

---
+++ ionpot/widget/padded_text.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "padded.hpp"
+#include "texture.hpp"
+
+namespace ionpot::widget {
+	using PaddedText = Padded<Texture>;
+}
---
+++ ionpot/widget/padded_text_box.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "padded_text.hpp"
+#include "text_box.hpp"
+
+namespace ionpot::widget {
+	using PaddedTextBox = TextBox<PaddedText>;
+}
