2a871bd840b61e5259a082d5b492ce422c827bf4
Emir Kotan <emirkotan@gmail.com>
2022-05-29 02:40:51 +0100

    rm dice::Output for now

--- ionpot/util/dice.hpp
+++ ionpot/util/dice.hpp
@@ -3,7 +3,6 @@
 #include "exception.hpp"
 
 #include <random>
-#include <vector>
 
 namespace ionpot::util::dice {
 	IONPOT_EXCEPTION("Dice")
@@ -13,23 +12,10 @@ namespace ionpot::util::dice {
 		int sides;
 	};
 
-	class Output {
-	public:
-		Output(const Input&);
-		void add(int);
-		int count() const;
-		const std::vector<int>& rolls() const;
-		int sides() const;
-		int total() const;
-	private:
-		int m_sides;
-		std::vector<int> m_rolls;
-	};
-
 	class Engine {
 	public:
 		Engine(unsigned int seed);
-		Output roll(const Input&);
+		int roll(const Input&);
 	private:
 		std::mt19937 m_engine;
 	};
