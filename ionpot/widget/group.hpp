0050e73fcc1fc7cf6d4ef90892ae3b2ad1a9c552
Emir Kotan <emirkotan@gmail.com>
2022-06-14 00:11:42 +0100

    widget::Group

---
+++ ionpot/widget/group.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "element.hpp"
+
+#include <util/point.hpp>
+#include <util/vector.hpp>
+
+#include <memory> // std::shared_ptr
+
+namespace ionpot::widget {
+	class Group : public Element {
+	public:
+		using Elements = util::PtrVector<Element>;
+		using Groups = util::PtrVector<Group>;
+
+		std::shared_ptr<Element>
+			find(util::Point, util::Point offset = {});
+
+		void render(util::Point offset = {}) const final;
+
+		void update_size();
+
+	protected:
+		void elements(Elements&&);
+		void groups(Groups&&);
+
+	private:
+		Elements m_elements;
+		Groups m_groups;
+	};
+}
